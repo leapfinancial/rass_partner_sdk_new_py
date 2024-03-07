18314658a37760d2f33b007315763da5b4ce45bb
Jorge Trujillo Perez <jorgeluistrujilllo@gmail.com>
2024-03-07 10:44:36 -0500

    new name

--- raassdkpy/api/__init__.py
+++
@@ -1,9 +0,0 @@
-# flake8: noqa
-
-# import apis into api package
-from raassdkpy.api.default_api import DefaultApi
-from raassdkpy.api.file_api import FileApi
-from raassdkpy.api.partner_api import PartnerApi
-from raassdkpy.api.partner_full_api import PartnerFullApi
-from raassdkpy.api.partner_send_api import PartnerSendApi
-
---
+++ raassdkpyv2/api/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+
+# import apis into api package
+from raassdkpyv2.api.default_api import DefaultApi
+from raassdkpyv2.api.file_api import FileApi
+from raassdkpyv2.api.partner_api import PartnerApi
+from raassdkpyv2.api.partner_full_api import PartnerFullApi
+from raassdkpyv2.api.partner_send_api import PartnerSendApi
+
