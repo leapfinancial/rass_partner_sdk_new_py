18923d99ea5b8d59b7aa9d4160c7f739844541e9
Andres Pinto <andres.pinto2203@gmail.com>
2024-05-21 16:23:31 -0400

    Update get_reference_code_response.py

--- raassdkpyv2/models/get_reference_code_response.py
+++ raassdkpyv2/models/get_reference_code_response.py
@@ -70,9 +70,12 @@ class GetReferenceCodeResponse(BaseModel):
         _obj = GetReferenceCodeResponse.parse_obj({
             "code": obj.get("code"),
             "expiration_date": obj.get("expirationDate"),
+            "expirationDate": obj.get("expirationDate"),
             "code_type": obj.get("codeType"),
+            "codeType": obj.get("codeType"),
             "status": obj.get("status"),
-            "status_message": obj.get("statusMessage")
+            "status_message": obj.get("statusMessage"),
+            "statusMessage": obj.get("statusMessage")
         })
         return _obj
 
